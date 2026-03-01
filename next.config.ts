5eeb14048ea61438993533c72956a26a7b461e32
qudo-code <qudo@matr.world>
2026-02-28 20:24:01 -0800

    rm stuff

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,5 @@
-import MillionLint from "@million/lint";
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  // turbopack: {} // silencing Next 16 turbopack warning caused by Million webpack inject
-};
+const nextConfig: NextConfig = {};
 
-export default MillionLint.next({
-  enabled: true,
-  rsc: true
-})(nextConfig);
+export default nextConfig;
